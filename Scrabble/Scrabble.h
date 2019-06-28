190197973e339221ec4d96d4dcbd5b54a3313919
Jose Fernando Rodriguez Minguet <josefrm90@gmail.com>
2019-06-28 07:07:30 +0200

    Initial Commit

---
+++ Scrabble/Scrabble.h
@@ -0,0 +1,19 @@
+//
+//  Scrabble.h
+//  Scrabble
+//
+//  Created by José Fernando Rodríguez Minguet on 28/06/2019.
+//  Copyright © 2019 José Fernando Rodríguez Minguet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Scrabble.
+FOUNDATION_EXPORT double ScrabbleVersionNumber;
+
+//! Project version string for Scrabble.
+FOUNDATION_EXPORT const unsigned char ScrabbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scrabble/PublicHeader.h>
+
+
